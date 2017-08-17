f753e84b2dc76b59815a8b2c7d6a212cfb740a2f
Yannick Heinrich <yannick.heinrich@gmail.com>
2017-08-17 16:32:37 +0200

    Compilation ok.

--- androidbuild/app/src/main/java/net/yageek/testapp/MainActivity.java
+++ androidbuild/app/src/main/java/net/yageek/testapp/MainActivity.java
@@ -1,13 +1,20 @@
-package yageek.net.cpp_ble;
+package net.yageek.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("cpp-ble");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
